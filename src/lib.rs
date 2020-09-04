ba0ee2add2f540cb3e9f424494281374118d9bc3
Dylan Staatz <dylanstaatz@gmail.com>
2020-09-04 18:37:00 -0400

    Added pwm based steering

--- src/lib.rs
+++ src/lib.rs
@@ -59,10 +59,6 @@ impl DrivetrainController {
         })
     }
 
-    pub fn set_output(&mut self, output: f64) -> gpio::Result<()> {
-        self.set_output_with_frequency(output, self.frequency)
-    }
-
     /// Sets the power and direction of the drivetrain at the given pwm frequency.
     /// 
     /// 'output' is a bounded on the interval `[-1.0, 1.0]`. Naturally
@@ -71,30 +67,59 @@ impl DrivetrainController {
     /// the interval `(0.0, 1.0]` to `(min_duty_cycle, 1.0]` while still
     /// allowing the setting of output to `0.0`.
     /// 
-    /// `frequency` is specified in hertz (Hz) and must be `>= 0`.
-    /// 
-    /// Will panic if output or frequency is NaN.
-    pub fn set_output_with_frequency(&mut self, output: f64, frequency: f64)
-        -> gpio::Result<()> {
-        
+    /// Will panic if output is NaN.
+    pub fn set_output(&mut self, output: f64) -> gpio::Result<()> {
+
         let output = output.max(-1.0).min(1.0);
-        let frequency = frequency.max(0.0);
 
         if output == 0.0 {
-            self.pwm_0.set_pwm_frequency(frequency, 0.0)?;
-            self.pwm_1.set_pwm_frequency(frequency, 0.0)?;
+            self.pwm_0.set_pwm_frequency(self.frequency, 0.0)?;
+            self.pwm_1.set_pwm_frequency(self.frequency, 0.0)?;
         } else if output > 0.0 {
             let output = linear_map(output, (0.0, 1.0), (self.min_duty_cycle, 1.0));
-            self.pwm_0.set_pwm_frequency(frequency, output)?;
-            self.pwm_1.set_pwm_frequency(frequency, 0.0)?;
+            self.pwm_0.set_pwm_frequency(self.frequency, output)?;
+            self.pwm_1.set_pwm_frequency(self.frequency, 0.0)?;
         } else {
             let output = linear_map(-1.0 * output, (0.0, 1.0), (self.min_duty_cycle, 1.0));
-            self.pwm_0.set_pwm_frequency(frequency, 0.0)?;
-            self.pwm_1.set_pwm_frequency(frequency, output)?;
+            self.pwm_0.set_pwm_frequency(self.frequency, 0.0)?;
+            self.pwm_1.set_pwm_frequency(self.frequency, output)?;
         }
 
         Ok(())
     }
+
+    // /// Sets the power and direction of the drivetrain at the given pwm frequency.
+    // /// 
+    // /// 'output' is a bounded on the interval `[-1.0, 1.0]`. Naturally
+    // /// a negative number specifies reverse and a posiive number specifies
+    // /// forward and `0.0` is no output. This function will correctly map
+    // /// the interval `(0.0, 1.0]` to `(min_duty_cycle, 1.0]` while still
+    // /// allowing the setting of output to `0.0`.
+    // /// 
+    // /// `frequency` is specified in hertz (Hz) and must be `>= 0`.
+    // /// 
+    // /// Will panic if output or frequency is NaN.
+    // pub fn set_output_with_frequency(&mut self, output: f64, frequency: f64)
+    //     -> gpio::Result<()> {
+        
+    //     let output = output.max(-1.0).min(1.0);
+    //     let frequency = frequency.max(0.0);
+
+    //     if output == 0.0 {
+    //         self.pwm_0.set_pwm_frequency(frequency, 0.0)?;
+    //         self.pwm_1.set_pwm_frequency(frequency, 0.0)?;
+    //     } else if output > 0.0 {
+    //         let output = linear_map(output, (0.0, 1.0), (self.min_duty_cycle, 1.0));
+    //         self.pwm_0.set_pwm_frequency(frequency, output)?;
+    //         self.pwm_1.set_pwm_frequency(frequency, 0.0)?;
+    //     } else {
+    //         let output = linear_map(-1.0 * output, (0.0, 1.0), (self.min_duty_cycle, 1.0));
+    //         self.pwm_0.set_pwm_frequency(frequency, 0.0)?;
+    //         self.pwm_1.set_pwm_frequency(frequency, output)?;
+    //     }
+
+    //     Ok(())
+    // }
 }
 
 
@@ -106,17 +131,65 @@ const STEERING_PWM1: pwm::Channel = pwm::Channel::Pwm1;
 
 
 struct SteeringController {
-    channel_0: pwm::Pwm,
-    channel_1: pwm::Pwm,
+    pwm_0: pwm::Pwm,
+    pwm_1: pwm::Pwm,
+    min_duty_cycle: f64,
 }
 
 impl SteeringController {
 
-    fn new() -> pwm::Result<Self> {
+    /// The following line will need to be added to last line in /boot/config.txt
+    /// ```txt
+    /// dtoverlay=pwm-2chan,pin=12,func=4,pin2=13,func2=4
+    /// ```
+    fn new(frequency: f64, min_duty_cycle: f64) -> pwm::Result<Self> {
+        
         Ok(Self {
-            channel_0: pwm::Pwm::new(STEERING_PWM0)?,
-            channel_1: pwm::Pwm::new(STEERING_PWM1)?,
+            pwm_0: pwm::Pwm::with_frequency(
+                STEERING_PWM0,
+                frequency.max(0.0),
+                0.0,
+                pwm::Polarity::Normal,
+                true,
+            )?,
+            pwm_1: pwm::Pwm::with_frequency(
+                STEERING_PWM1,
+                frequency.max(0.0),
+                0.0,
+                pwm::Polarity::Normal,
+                true,
+            )?,
+            min_duty_cycle: min_duty_cycle.max(0.0).min(1.0),
         })
+    }
+
+    /// Sets the power and direction of the drivetrain at the given pwm frequency.
+    /// 
+    /// 'output' is a bounded on the interval `[-1.0, 1.0]`. A positive number
+    /// will steering the car to the right and a negative number will steering
+    /// the car to the left while `0.0` will go straight. This function will
+    /// correctly map the interval `(0.0, 1.0]` to `(min_duty_cycle, 1.0]`
+    /// while still allowing the setting of output to `0.0`.
+    /// 
+    /// Will panic if output is NaN.
+    pub fn set_output(&mut self, output: f64) -> pwm::Result<()> {
+
+        let output = output.max(-1.0).min(1.0);
+
+        if output == 0.0 {
+            self.pwm_0.set_duty_cycle(0.0)?;
+            self.pwm_1.set_duty_cycle(0.0)?;
+        } else if output > 0.0 {
+            let output = linear_map(output, (0.0, 1.0), (self.min_duty_cycle, 1.0));
+            self.pwm_0.set_duty_cycle(output)?;
+            self.pwm_1.set_duty_cycle(0.0)?;
+        } else {
+            let output = linear_map(-1.0 * output, (0.0, 1.0), (self.min_duty_cycle, 1.0));
+            self.pwm_0.set_duty_cycle(0.0)?;
+            self.pwm_1.set_duty_cycle(output)?;
+        }
+
+        Ok(())
     }
 }
 
@@ -142,10 +215,21 @@ impl TigerCar {
         
         Self {
             steering: DrivetrainController::new(40.0, 0.3).unwrap(),
-            drivetrain: SteeringController::new().unwrap(),
+            drivetrain: SteeringController::new(50.0, 0.5).unwrap(),
         }
     }
 
+    pub fn stop(&mut self) {
+        self.steering.set_output(0.0).unwrap();
+        self.drivetrain.set_output(0.0).unwrap();
+    }
+
+}
+
+impl Drop for TigerCar {
+    fn drop(&mut self) {
+        self.stop()
+    }
 }
 
 
@@ -184,8 +268,17 @@ fn run_steering() {
     controller.set_output(0.0).unwrap();
 }
 
+fn run_tiger() {
+    let mut tiger = TigerCar::new();
+
+    tiger.steering.set_output(0.5).unwrap();
+    tiger.drivetrain.set_output(0.5).unwrap();
+    sleep(Duration::new(1, 0));
+    tiger.stop();
+}
+
 pub fn run() {
-    run_drivetrain()
+    run_tiger()
 }
 
 
@@ -202,15 +295,5 @@ mod tests {
         assert_eq!(linear_map(6.0, (0.0, 10.0), (0.0, 100.0)), 60.0);
         assert_eq!(linear_map(6.0, (0.0, 10.0), (10.0, 20.0)), 16.0);
         assert_eq!(linear_map(6.0, (0.0, 10.0), (5.0, 10.0)), 8.0);
-    }
-
-    #[test]
-    fn test_full_drivetrain_scale() {
-        DrivetrainController::new(60.0, 0.5).unwrap();
-    }
-
-    #[test]
-    fn test_full_steering_scale() {
-        unimplemented!();
     }
 }
