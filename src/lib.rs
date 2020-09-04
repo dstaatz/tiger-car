06306100c9a7f2bd4e9ca19ff0e8e1f8dadfc82e
Dylan Staatz <dylanstaatz@gmail.com>
2020-09-04 01:46:45 -0400

    First diretrain implementation builds

--- src/lib.rs
+++ src/lib.rs
@@ -7,14 +7,15 @@ use rppal::pwm;
 
 /// Maps the input a from the interval a to the interval b.
 /// No checks are done on bounds.
-fn linear_map(intput: f64, a: (f64, f64), b: (f64, f64)) -> f64 {
-    (input - a.0) * (b.1 - b.0) / (a.1 - a.0) + b.0;
+fn linear_map(input: f64, a: (f64, f64), b: (f64, f64)) -> f64 {
+    (input - a.0) * (b.1 - b.0) / (a.1 - a.0) + b.0
 }
 
 
 ////////////////////////////////////////////////////////////////////////////////
 
 
+// Pins
 const DRIVETRAIN_PWM0: u8 = 5;
 const DRIVETRAIN_PWM1: u8 = 6;
 
@@ -41,8 +42,8 @@ impl DrivetrainController {
     /// the interval `(0.0, 1.0]` to `(min_duty_cycle, 1.0]` while still
     /// allowing the setting of output to `0.0`.
     ///     
-    /// Implemented with software pwm, therefore higher freuquencies may not
-    /// work. Additionally, generally performance may not be consistent.
+    /// Implemented with software PWMs, therefore higher freuquencies may not
+    /// work. Additionally, general performance may not be consistent.
     fn new(frequency: f64, min_duty_cycle: f64) -> gpio::Result<Self> {
         
         let gpio = gpio::Gpio::new()?;
@@ -58,9 +59,8 @@ impl DrivetrainController {
         })
     }
 
-
-    pub fn set_output(&mut self, output: f64) {
-        self.set_output_with_frequency(output, self.frequency);
+    pub fn set_output(&mut self, output: f64) -> gpio::Result<()> {
+        self.set_output_with_frequency(output, self.frequency)
     }
 
     /// Sets the power and direction of the drivetrain at the given pwm frequency.
@@ -74,30 +74,35 @@ impl DrivetrainController {
     /// `frequency` is specified in hertz (Hz) and must be `>= 0`.
     /// 
     /// Will panic if output or frequency is NaN.
-    pub fn set_output_with_frequency(&mut self, output: f64, frequency: f64) {
+    pub fn set_output_with_frequency(&mut self, output: f64, frequency: f64)
+        -> gpio::Result<()> {
         
         let output = output.max(-1.0).min(1.0);
         let frequency = frequency.max(0.0);
 
         if output == 0.0 {
-            self.pwm_0.set_pwm_frequency(frequency, 0.0);
-            self.pwm_1.set_pwm_frequency(frequency, 0.0);
+            self.pwm_0.set_pwm_frequency(frequency, 0.0)?;
+            self.pwm_1.set_pwm_frequency(frequency, 0.0)?;
         } else if output > 0.0 {
             let output = linear_map(output, (0.0, 1.0), (self.min_duty_cycle, 1.0));
-
+            self.pwm_0.set_pwm_frequency(frequency, output)?;
+            self.pwm_1.set_pwm_frequency(frequency, 0.0)?;
         } else {
-
+            let output = linear_map(-1.0 * output, (0.0, 1.0), (self.min_duty_cycle, 1.0));
+            self.pwm_0.set_pwm_frequency(frequency, 0.0)?;
+            self.pwm_1.set_pwm_frequency(frequency, output)?;
         }
-    }
 
+        Ok(())
+    }
 }
 
 
 ////////////////////////////////////////////////////////////////////////////////
 
 
-const STEERING_PWM0 = pwm::Channel::Pwm0;
-const STEERING_PWM1 = pwm::Channel::Pwm1;
+const STEERING_PWM0: pwm::Channel = pwm::Channel::Pwm0;
+const STEERING_PWM1: pwm::Channel = pwm::Channel::Pwm1;
 
 
 struct SteeringController {
@@ -109,11 +114,14 @@ impl SteeringController {
 
     fn new() -> pwm::Result<Self> {
         Ok(Self {
-            channel_0: pwm::Pwm::new(DRIVETRAIN_PWM0)?,
-            channel_1: pwm::Pwm::new(DRIVETRAIN_PWM1)?,
+            channel_0: pwm::Pwm::new(STEERING_PWM0)?,
+            channel_1: pwm::Pwm::new(STEERING_PWM1)?,
         })
     }
 }
+
+
+////////////////////////////////////////////////////////////////////////////////
 
 
 /// TODO
@@ -151,6 +159,18 @@ mod tests {
 
     #[test]
     fn test_linear_map() {
-        assert_eq!(linear_map(6.0, (0.0, 10.0), (0.0, 100.0)), 6.0);
+        assert_eq!(linear_map(6.0, (0.0, 10.0), (0.0, 100.0)), 60.0);
+        assert_eq!(linear_map(6.0, (0.0, 10.0), (10.0, 20.0)), 16.0);
+        assert_eq!(linear_map(6.0, (0.0, 10.0), (5.0, 10.0)), 8.0);
+    }
+
+    #[test]
+    fn test_full_drivetrain_scale() {
+        unimplemented!();
+    }
+
+    #[test]
+    fn test_full_steering_scale() {
+        unimplemented!();
     }
 }
